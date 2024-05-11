5ea2665c3c030aa4e61dcb38101a76b30e6b1c20
Josh Stringer <joshstringer@live.com>
2024-05-11 12:18:20 -0500

    left aligned form text and added partial functionality to button2 (it now collects info and puts it into an object and pushed it to an array

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,8 @@
 const button = document.getElementById('button');
+const button2 = document.getElementById('button2');
+const inputBox = document.querySelector('.inputBox');
+const inputBox2 = document.querySelector('.inputBox2');
+const inputBox3 = document.querySelector('.inputBox3');
 const page = document.querySelector('body');
 
 let darkMode = false;
@@ -11,3 +15,26 @@ button.addEventListener('click', function () {
         page.classList.remove('darkMode');
     }
 });
+
+button2.addEventListener('click', function () {
+
+
+    let blogArray = [];
+
+    const User = inputBox.value;
+    const Title = inputBox2.value;
+    let Content = inputBox3.value;
+
+    let infoObject = {
+        User: User,
+        Title: Title,
+        Content: Content
+    };
+
+    blogArray.push(infoObject);
+
+    console.log(blogArray);
+
+    return;
+
+})
