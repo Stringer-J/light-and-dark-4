05e5d3f64bb08e8c5c010360e95e8c47ab2d46ab
Josh Stringer <joshstringer@live.com>
2024-05-18 14:18:58 -0500

    made the CSS a little better, updated the README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # light-and-dark-4
+
+
+Description: This is a repo for edits and fixes made to a webpage for educational purposes.
+
+Installation:
+N/A
+
+Usage:
+To use this repo, 
+
+Link to site:
+
+
+Credits:
+N/A
+
+License:
+Refer to the LICENSE in the repo.
+
+![lightOne](./assets/images/image-1.png)
+![darkOne](./assets/images/image-2.png)
+![lightTwo](./assets/images/image-5.png)
+![darkTwo](./assets/images/image-6.png)
--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,6 +2,7 @@ const button3 = document.getElementById('button3');
 const savedObjectInfo = JSON.parse(localStorage.getItem('objectInfo'));
 const container = document.querySelector('.containerPage2');
 
+// loads the saved posts array and updates it with a new object
 function updateSavedPosts() {
     const savedPosts = JSON.parse(localStorage.getItem('savedPosts')) || [];
     savedPosts.push(savedObjectInfo);
@@ -24,13 +25,13 @@ document.addEventListener('DOMContentLoaded', function () {
     updateDarkMode();
 });
 
-// adds a 'back' button to return to previous page and uses function to save posts to local storage
+// adds functionality to the 'back' button to return to previous page and uses function to save posts to local storage
 button3.addEventListener('click', function () {
     window.location.href = 'index.html';
     updateSavedPosts();
 });
 
-//take object saved to local storage and posts in formatted inside of a box
+//take object saved to local storage and posts it formatted inside of a box
 if (savedObjectInfo && container) {
 
     const titleInfo = savedObjectInfo.Title;
@@ -39,36 +40,44 @@ if (savedObjectInfo && container) {
 
     if (userInfo && titleInfo && contentInfo) {
 
+    // creates box
     const newDiv = document.createElement('div');
     newDiv.classList.add('newPost');
 
+    // creates title text in box
     const newTitle = document.createElement('h3');
     newTitle.textContent = `${titleInfo}`;
     newDiv.appendChild(newTitle);
 
+    // creates line under title text
     const line = document.createElement('hr');
     line.classList.add('newPostLine');
     newDiv.appendChild(line);
 
+    // creates text using the content info
     const newContent = document.createElement('p');
     newContent.textContent = `${contentInfo}`;
     newDiv.appendChild(newContent);
 
+    // creates text saying what user posted the entry
     const newUser = document.createElement('h4');
     newUser.classList.add('userText');
     newUser.textContent = `Posted By: ${userInfo}`;
     newDiv.appendChild(newUser);
 
+    // puts finished box on page
     container.appendChild(newDiv);
 }};
 
-
+// creates a new entry for each post already saved previously
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.querySelector('.containerPage2');
     let savedPosts = JSON.parse(localStorage.getItem('savedPosts')) || [];
 
+    // loads the posts in reverse so that the newest is at the top
     savedPosts = savedPosts.reverse();
 
+    // does everything above but for each object in the savedPosts array
     savedPosts.forEach(function(post) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('newPost');
